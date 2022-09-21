035eecdabf9d260719433162573221829fdb7055
yangorev <yan-gorev@mail.ru>
2022-09-22 01:00:44 +0300

    refactor first task

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class First_task():
 
         return iteraction_func
 
-
+#check unimodality
     def check_unim(self, arr, func):
         first_item = arr[0]
         last_item = arr[-1]
@@ -62,11 +62,11 @@ class First_task():
         checker = self.check_unim(arr, func)
         step = self.accuracy()
         function = self.choose_task(task)
-        iteractions = 0
+        iterations = 0
 
         if checker == True:
             for num_checker in np.arange(first_item, last_item, step):
-                iteractions += 1
+                iterations += 1
                 res_x = arr[0] + num_checker * step
 
                 res_y = function(res_x)
@@ -74,7 +74,7 @@ class First_task():
                 left.append(res_x)
                 right.append(res_y)
 
-            print(f"min : {min(right)} and iteractions: {iteractions}")
+            print(f"f-calculation : {min(right)} and iterations: {iterations}")
         else:
             return False
 
@@ -84,13 +84,13 @@ class First_task():
         last_item = arr[-1]
         checker = self.check_unim(arr, func)
         midpoint = eps / 2
-        iteractions = 0
+        iterations = 0
         function = self.choose_task(task)
 
         if checker == True:
 
             while abs(last_item - first_item) / 2 > eps:
-                iteractions += 1
+                iterations += 1
 
                 x1 = (first_item + last_item - midpoint) / 2
                 x2 = (first_item + last_item + midpoint) / 2
@@ -103,14 +103,14 @@ class First_task():
             x_mid = (first_item + last_item) / 2
             func_x = function(x_mid)
 
-            print(x_mid, func_x, iteractions)
+            print(f"f-calculation : {func_x} and iterations: {iterations}")
 
         else:
             return False
 
 
     def result_golden(self, arr, func, task):
-        iteractions = 0
+        iterations = 0
         eps = self.accuracy()
         first_item = arr[0]
         last_item = arr[-1]
@@ -121,7 +121,7 @@ class First_task():
 
             while 0.61*abs(last_item - first_item) > eps:
 
-                iteractions += 1
+                iterations += 1
 
                 x1 = first_item + 0.38*(last_item - first_item)
                 x2 = first_item + 0.61*(last_item - first_item)
@@ -134,14 +134,15 @@ class First_task():
             x_mid = (first_item + last_item) / 2
             func_x = function(x_mid)
 
-            print(x_mid, func_x, iteractions)
+            print(f"f-calculation : {func_x} and iterations: {iterations}")
 
         else:
             return False
 
 
+#ex
 q = First_task()
 x = Symbol('x')
-res_dio = q.result_dioch([0, 1], x**3, "degree")
 res_num = q.result_num([0, 1], x**3, "degree")
+res_dio = q.result_dioch([0, 1], x**3, "degree")
 res_gold = q.result_golden([0, 1], x**3, "degree")
